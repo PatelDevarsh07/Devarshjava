735205b8a35234ed84e85b9c23cc97952b2f5599
PatelDevarsh07 <devutpatel0403@gmail.com>
2022-09-10 08:44:40 -0700

    add file

---
+++ j2house.java
@@ -0,0 +1,14 @@
+package java1;
+
+public class j2house {
+
+	public static void main(String[] args) {
+		System.out.println("      *     ");
+		System.out.println("    * * *   ");
+		System.out.println("    * * *   ");
+		System.out.println("    *   *   ");
+		System.out.println("    *   *   ");
+		System.out.println("    *****   ");
+	}
+
+}
---
+++ j3border.java
@@ -0,0 +1,13 @@
+package java1;
+
+public class j3border {
+
+	public static void main(String[] args) {
+		System.out.println("   * * * *     ");
+		System.out.println("   *     *     ");
+		System.out.println("   *     *     ");
+		System.out.println("   * * * *     ");
+
+	}
+
+}
