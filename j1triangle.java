2edfee2549719ef9c95faea62c2929af2fba7ac9
PatelDevarsh07 <devutpatel0403@gmail.com>
2022-09-10 08:40:49 -0700

    add file

---
+++ j1triangle.java
@@ -0,0 +1,14 @@
+package java1;
+
+import java.util.Scanner;
+
+public class j1triangle {
+
+	public static void main(String[] args) {
+		System.out.println("     *    ");
+		System.out.println("    * *   ");
+		System.out.println("   * * *  ");
+		System.out.println("  * * * * ");
+	}
+}
+
